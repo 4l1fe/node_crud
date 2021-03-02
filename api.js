b8d0090d62caed8c9fbbc326da491c69b75217c2
4l1fe <4l1fe@users.noreply.github.com>
2021-03-02 16:08:16 +0700

    Create method

--- api.js
+++ api.js
@@ -1,14 +1,30 @@
 var restify = require('restify');
+var levelup = require('levelup');
+var leveldown = require('leveldown');
 
-function respond(req, res, next) {
-    res.send('hello ' + req.params.name);
+var db = levelup(leveldown('./mydb.lvl'))
+
+function create(req, res, next) {
+    let key = req.body.field;
+    let value = req.body.value;
+
+    console.log('put data:', key, value)
+    db.put(key, value, {errorIfExists: true}, function (err) {
+        if (err) {
+            res.send('Record already exists');
+        } else {
+            res.send('Record is created');
+        }        
+    })
+    console.log('complete req')
     next();
-  }
+}
   
-  var server = restify.createServer();
-  server.get('/hello/:name', respond);
-  server.head('/hello/:name', respond);
-  
-  server.listen(8080, function() {
+var server = restify.createServer();
+server.use(restify.plugins.bodyParser({ mapParams: true }));
+
+server.post('/todo', create);
+
+server.listen(8080, '127.0.0.1', function() {
     console.log('%s listening at %s', server.name, server.url);
-  });
+});
