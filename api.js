792376f2db305877f3acd863f3c0a0dcbc6ca9c1
4l1fe <4l1fe@users.noreply.github.com>
2021-03-02 21:42:00 +0700

    Create method Promise chain

--- api.js
+++ api.js
@@ -8,23 +8,28 @@ function create(req, res, next) {
     let key = req.body.field;
     let value = req.body.value;
 
-    console.log('Put data:', key, value)
-    db.get(key, function(err, value) {
-        if (err) {
-            res.send(err);
-        } else if (value) {
-            res.send('Record already exists');
+    db.get(key)
+    .then(value => {
+        if (value) {
+            return next(new Error('Record already exists!'));
         }
     })
-    
-    db.put(key, value, function (err) {
-        if (err) {
-            res.send(err);   
-        } else {
-            res.send('Record is created');
-        }        
+    .catch(error => {
+        if (!error.notFound) {
+            return next(error);
+        }
     })
-    console.log('Complete Req')
+    .then(() => {
+        console.log('put data')
+        return db.put(key, value)
+    })
+    .then(() => {
+        res.send('Record is created');
+    })
+    .catch(error => {
+        return next(error);
+    });
+
     next();
 }
 
@@ -36,8 +41,8 @@ function read(req, res, next) {
         .on('data', function (data) {
             records.push([data.key.toString(), data.value.toString()])
         })
-        .on('error', function (err) {
-            res.send(err)
+        .on('error', function (error) {
+            res.send(error)
         })
         .on('close', function () {
             res.send(records);
@@ -50,31 +55,27 @@ function update(req, res, next) {
     let key = req.body.field;
     let value = req.body.value;
 
-    console.log('Update data:', key, value)
-    db.put(key, value, function (err) {
-        if (err) {
-            res.send('Record already exists');
+    db.put(key, value, function (error) {
+        if (error) {
+            return next(error);
         } else {
             res.send('Record is updated');
         }        
     })
-    console.log('Complete Req')
     next();
 }
 
 function delete_(req, res, next) {
     let key = req.params.field;
 
-    console.log('Delete field:', key)
-    db.del(key, function (err) {
-        if (err) {
-            res.send(err);
+    db.del(key, function (error) {
+        if (error) {
+            return next(error);
         } else {
             res.send('Deleted');
         }
     })
     
-    console.log('Complete Req')
     next();
 }
 
